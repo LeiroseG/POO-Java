7044620ee2c9a5f7fe68722af43e8c011e0e3d27
Gabriel Fonseca <gabrielfonseca555@gmail.com>
2026-04-12 15:49:51 -0300

    Atualizando para interface

--- Interface/src/application/ComInterface.java
+++ Interface/src/application/ComInterface.java
@@ -9,14 +9,12 @@ import model.entities.Vehicle;
 import model.services.BrazilTaxService;
 import model.services.RentalService;
 
-public class SemInterface {
-	public static void main(String[]args ) {
-		
+public class ComInterface {
+	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		
-		
-		
+
 		System.out.println("Entre com os dados do aluguel ");
 		System.out.print("Modelo do Carro ");
 		String carModel = sc.nextLine();
@@ -24,29 +22,24 @@ public class SemInterface {
 		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
 		System.out.print("Retorno : ");
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
-		
-		CarRental cr= new CarRental(start,finish,new Vehicle(carModel));
-		
-		System.out.print("Entre o preço por hora : " );
+
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+		System.out.print("Entre o preço por hora : ");
 		double pricePerHour = sc.nextDouble();
-		System.out.print("Entre o preço por dia : " );
+		System.out.print("Entre o preço por dia : ");
 		double pricePerDay = sc.nextDouble();
-		
-		
-		RentalService rentalService = new RentalService(pricePerDay,pricePerHour, new BrazilTaxService());
-		
+
+		RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+
 		rentalService.processInvoice(cr);
-		
+
 		System.out.println("Fatura : ");
 		System.out.println("Pagamento basico :  : " + cr.getInvoice().getBasicPayment());
-		System.out.println("Imposto : "+ cr.getInvoice().getTax());
+		System.out.println("Imposto : " + cr.getInvoice().getTax());
 		System.out.println("Pagamento total  : " + cr.getInvoice().getTotalPayment());
-		
-		
-		
-		
-		
+
 		sc.close();
-		
+
 	}
 }
--- Interface/src/model/services/RentalService.java
+++ Interface/src/model/services/RentalService.java
@@ -8,10 +8,10 @@ import model.entities.Invoice;
 public class RentalService {
 	private Double pricePerDay;
 	private Double pricePerHour;
-	
-	private BrazilTaxService taxService;
 
-	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+	private TaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
 		super();
 		this.pricePerDay = pricePerDay;
 		this.pricePerHour = pricePerHour;
@@ -20,17 +20,17 @@ public class RentalService {
 
 	public void processInvoice(CarRental carRental) {
 		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
-		double hours = minutes /60;
+		double hours = minutes / 60;
 		double basicPayment;
-		
-		if(hours <= 12) {
+
+		if (hours <= 12) {
 			basicPayment = pricePerHour * Math.ceil(hours);
-		}else {
-			basicPayment = pricePerDay * Math.ceil(hours/24);
+		} else {
+			basicPayment = pricePerDay * Math.ceil(hours / 24);
 		}
-		
+
 		double tax = taxService.tax(basicPayment);
 		carRental.setInvoice(new Invoice(basicPayment, tax));
 	}
-	
+
 }
